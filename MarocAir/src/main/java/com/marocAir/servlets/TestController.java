60a89ae06a30d6edbb5c4c5a3c97661f96ff4197
Amine Mesbahi <mesbahiamine886@gmail.com>
2022-10-12 12:13:04 +0100

    Add tailwind dependencies to the project

--- MarocAir/src/main/java/com/marocAir/servlets/TestController.java
+++ MarocAir/src/main/java/com/marocAir/servlets/TestController.java
@@ -10,8 +10,10 @@ import java.io.IOException;
 
 @WebServlet(urlPatterns = "/test")
 public class TestController extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("index.jsp").forward(req, resp);
+        String name = req.getParameter("name");
+        req.getRequestDispatcher("/views/index.jsp").forward(req, resp);
     }
 }
---
+++ MarocAir/target/MarocAir/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.{html,js,jsp}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
