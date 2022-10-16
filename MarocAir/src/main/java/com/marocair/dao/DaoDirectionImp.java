4603af7668df4c4b1abe391c7cb0b4302505aa6b
nemliProg <younesbuzz@gmail.com>
2022-10-16 17:32:07 +0100

    createDirections method created in DaoDirectionImp Class

--- MarocAir/src/main/java/com/marocair/dao/DaoDirectionImp.java
+++ MarocAir/src/main/java/com/marocair/dao/DaoDirectionImp.java
@@ -10,6 +10,16 @@ public class DaoDirectionImp extends Dao implements DaoDirection   {
     public DaoDirectionImp() throws ClassNotFoundException, SQLException {
     }
 
+    public Boolean createDirections(String firstCity, String secondCity) throws SQLException {
+        PreparedStatement st = this.conn.prepareStatement("INSERT INTO direcions(depart,arrive) VALUES(?,?),(?,?)");
+        st.setString(1, firstCity);
+        st.setString(2, secondCity);
+        st.setString(3, secondCity);
+        st.setString(4, firstCity);
+        Boolean result = st.execute();
+        return !result;
+    }
+
     public int[] selectDirection(String depart, String arrive) throws SQLException {
         PreparedStatement st = this.conn.prepareStatement("SELECT * FROM directions WHERE (depart = ? AND arrive = ?) OR (depart = ? AND arrive = ?)");
         st.setString(1,depart);
