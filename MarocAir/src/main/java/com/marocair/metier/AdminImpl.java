870a66e1167d9ac70208aee412ff5f6902c14128
nemliProg <younesbuzz@gmail.com>
2022-10-16 19:35:39 +0100

    createDirections method created in metier/AdminImpl Class

--- MarocAir/src/main/java/com/marocair/metier/AdminImpl.java
+++ MarocAir/src/main/java/com/marocair/metier/AdminImpl.java
@@ -1,7 +1,21 @@
 package com.marocair.metier;
 
+import com.marocair.dao.DaoDirectionImp;
+
+import java.sql.SQLException;
+
 public class AdminImpl {
     public AdminImpl(){}
 
+    public void createVols(String depart, String arrive, String escale) {
+    }
 
+    public String createDirections(String firstCity, String secondCity) throws SQLException, ClassNotFoundException {
+        DaoDirectionImp DDI = new DaoDirectionImp();
+        Boolean res = DDI.createDirections(firstCity,secondCity);
+        if (res)
+            return "Directions added successfully";
+        else
+            return "Something went wrong";
+    }
 }
