e26f494a70f918057856bfb7ac8f8c915ba29240
nemliProg <younesbuzz@gmail.com>
2022-10-17 11:49:48 +0100

    signature of selectAllDirections method

--- MarocAir/src/main/java/com/marocair/dao/DaoDirection.java
+++ MarocAir/src/main/java/com/marocair/dao/DaoDirection.java
@@ -1,7 +1,12 @@
 package com.marocair.dao;
 
+import com.marocair.model.Direction;
+
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public interface DaoDirection {
         int[] selectDirection(String depart, String arrive) throws SQLException;
+
+        ArrayList<Direction> selectAllDirections () throws SQLException;
 }
